09c0797a1d6462bf77cca31aabf558244fbdc698
Alex Steiner <alex.steiner@posteo.de>
2017-01-10 00:33:33 +0100

    rebased to forecastiopy3, manual geopy, breaker caching

--- weather.py
+++ weather.py
@@ -1,7 +1,34 @@
-from darksky import forecast
 from secrets import key
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+import argparse
+import pickle #to test smth
+from forecastiopy import *
+from geopy.geocoders import Nominatim
 
-fo = forecast(key, "Ljubljana", units="si")
-print(fo.temperature)
-print(fo.summary)
-print(fo.daily.summary)
+geolocator = Nominatim()
+
+parser = argparse.ArgumentParser(description="Small weather script.")
+parser.add_argument('location', type=str, help="for which to find the weather for")
+
+args = parser.parse_args()
+
+cache_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': '/tmp/weatherpycache/data',
+    'cache.lock_dir': '/tmp/weatherpycache/lock'
+}
+
+cache = CacheManager(**parse_cache_config_options(cache_opts))
+
+@cache.cache("forecast", expire=600)
+def get_forecast(locstr):
+    location = geolocator.geocode(locstr)
+    return ForecastIO.ForecastIO(key, latitude=location.latitude, longitude=location.longitude, units="si")
+
+fo = get_forecast(args.location)
+print(fo.currently["temperature"])
+print(fo.currently["summary"])
+print(fo.daily["summary"])
+
+fh = open("sth", "wb")
